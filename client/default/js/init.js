85c3062a9dd6d2c3b28d79d4178eb48db67e289c
sandip.limbachiya <sandip2451984@gmail.com>
2013-07-16 14:33:03 +0530

    Add Jquery Mobile css

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -8,8 +8,11 @@ call which will return the remote config.
 
 $fh.ready(function() {
   // The local config variable from config.js can be accessed directly
+  
+  
   document.getElementById('content2').innerHTML = "<p>" + JSON.stringify(config) + "</p>";
-
+  
+  
   document.getElementById('run_button').onclick = function() {
     // Invoke a cloud action call to get the remote configuration
     // See: http://docs.feedhenry.com/wiki/Actions
